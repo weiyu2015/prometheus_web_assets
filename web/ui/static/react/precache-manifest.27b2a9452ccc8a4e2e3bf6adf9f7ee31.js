3021d9032f4abb7c9783c5b4879551337334bd52
Wei Yu <weiyu@Weis-MacBook-Pro.local>
2021-01-15 16:49:16 -0500

    init

---
+++ web/ui/static/react/precache-manifest.27b2a9452ccc8a4e2e3bf6adf9f7ee31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3bcb792573ccfcd8c3570d8d8dca7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "899604b8c19da2ae5899",
+    "url": "./static/css/2.df42c974.chunk.css"
+  },
+  {
+    "revision": "7aa49ac03947bdac4bcb",
+    "url": "./static/css/main.a6053568.chunk.css"
+  },
+  {
+    "revision": "899604b8c19da2ae5899",
+    "url": "./static/js/2.0eebbff3.chunk.js"
+  },
+  {
+    "revision": "f61d6678639e237d4f330d830522a0f2",
+    "url": "./static/js/2.0eebbff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa49ac03947bdac4bcb",
+    "url": "./static/js/main.3f9caea1.chunk.js"
+  },
+  {
+    "revision": "b95e319c37fa43350336",
+    "url": "./static/js/runtime-main.5db206b5.js"
+  }
+]);
